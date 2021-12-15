c3a82902ee7bf8cd01f0d84ad24835a72343f96f
danyrose <danyrose1995@gmail.com>
2021-12-15 14:52:04 +0100

    ajout commande et ligne lommande

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,28 @@ class Article
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Categorie::class, inversedBy="articles")
+     */
+    private $categories;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Avis::class, mappedBy="article")
+     */
+    private $avis;
+
+    /**
+     * @ORM\OneToMany(targetEntity=LigneCommande::class, mappedBy="article")
+     */
+    private $lignesCommande;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+        $this->avis = new ArrayCollection();
+        $this->lignesCommande = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -69,6 +93,90 @@ class Article
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Categorie[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Categorie $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Categorie $category): self
+    {
+        $this->categories->removeElement($category);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Avis[]
+     */
+    public function getAvis(): Collection
+    {
+        return $this->avis;
+    }
+
+    public function addAvi(Avis $avi): self
+    {
+        if (!$this->avis->contains($avi)) {
+            $this->avis[] = $avi;
+            $avi->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvi(Avis $avi): self
+    {
+        if ($this->avis->removeElement($avi)) {
+            // set the owning side to null (unless already changed)
+            if ($avi->getArticle() === $this) {
+                $avi->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|LigneCommande[]
+     */
+    public function getLignesCommande(): Collection
+    {
+        return $this->lignesCommande;
+    }
+
+    public function addLignesCommande(LigneCommande $lignesCommande): self
+    {
+        if (!$this->lignesCommande->contains($lignesCommande)) {
+            $this->lignesCommande[] = $lignesCommande;
+            $lignesCommande->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLignesCommande(LigneCommande $lignesCommande): self
+    {
+        if ($this->lignesCommande->removeElement($lignesCommande)) {
+            // set the owning side to null (unless already changed)
+            if ($lignesCommande->getArticle() === $this) {
+                $lignesCommande->setArticle(null);
+            }
+        }
 
         return $this;
     }
