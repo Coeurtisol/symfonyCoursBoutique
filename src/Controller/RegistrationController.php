20192bd2df09b6eef003578fdefc884c0737eec4
Choquet charles <chqtcharles@gmail.com>
2022-01-20 11:20:13 +0100

    wip : wip

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,13 +10,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use App\Security\LoginAuthenticator;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $authenticator, 
+    LoginAuthenticator $formAuthenticator): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -35,7 +38,10 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('accueil');
+            return $authenticator->authenticateUser(
+                $user, 
+                $formAuthenticator, 
+                $request); 
         }
 
         return $this->render('registration/register.html.twig', [
